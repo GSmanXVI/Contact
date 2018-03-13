250b05c37f3da131a1a1abb129a2af845bb94758
Tural Muradov <Tural_M@hotmail.com>
2018-03-14 01:58:33 +0400

    ..

--- WpfApp10/MainWindow.xaml.cs
+++ WpfApp10/MainWindow.xaml.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Threading;
-using System.Reflection;
 using System.IO;
 using System.Xml.Serialization;
 using System.Collections.ObjectModel;
